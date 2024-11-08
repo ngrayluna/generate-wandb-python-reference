84124d61d8dd20369fa0b5d0f68b178c21e3c767
Noah Luna <15202580+ngrayluna@users.noreply.github.com>
2024-11-08 15:04:18 -0800

    Updated bash script

--- sdk_testing.sh
+++ sdk_testing.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 
+TEMP_DIR=sdk_docs_temp
+
 python generate_sdk_docs.py 
 
-python process_sdk_markdown.py --output_directory="sdk_docs_temp"
+python process_sdk_markdown.py --output_directory=$TEMP_DIR
 
-
-#cp Artifact.md /Users/noahluna/Desktop/docodile/docs/ref/lazy_docs_python
+cp $TEMP_DIR/*.md /Users/noahluna/Desktop/docodile/docs/ref/lazy_docs_python
