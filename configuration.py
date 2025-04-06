e521d11c19a411e6176aead5bb79baf2810406b4
Noah Luna <15202580+ngrayluna@users.noreply.github.com>
2025-04-06 13:14:29 -0700

    Replaces hardcode strings with dictionary

---
+++ configuration.py
@@ -0,0 +1,34 @@
+SOURCE = {
+    "SDK": {
+        "module": "wandb",
+        "file_path": "/Users/noahluna/Documents/GitHub/wandb/wandb/__init__.template.pyi",
+        "hugo_specs" : {
+            "frontmatter": "object_type: api",
+            "folder_name": "actions",
+        },
+    },
+    "DATATYPE": {
+        "module": "wandb",
+        "file_path": "/Users/noahluna/Documents/GitHub/wandb/wandb/__init__.template.pyi",
+        "hugo_specs": {
+            "frontmatter": "object_type: data-type",
+            "folder_name": "data-type",
+        },
+    },
+    "PUBLIC_API": {
+        "module": "wandb.apis.public",
+        "file_path": "/Users/noahluna/Documents/GitHub/wandb/wandb/apis/public/__init__.py",
+        "hugo_specs": {
+            "frontmatter": "object_type: public_apis_namespace",
+            "folder_name": "public-api",
+        },
+    },
+    "LAUNCH_API": {
+        "module": "wandb.sdk.launch",
+        "file_path": "/Users/noahluna/Documents/GitHub/wandb/wandb/sdk/launch/__init__.py",
+        "hugo_specs": {
+            "frontmatter": "object_type: launch_apis_namespace",
+            "folder_name": "launch-library",
+        },
+    },
+}
