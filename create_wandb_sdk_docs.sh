46ebc62dfa1b52e9b548f306aa7e18222d636923
Noah Luna <15202580+ngrayluna@users.noreply.github.com>
2025-06-05 20:29:00 -0700

    uodate bash script

--- create_wandb_sdk_docs.sh
+++ create_wandb_sdk_docs.sh
@@ -2,32 +2,22 @@
 
 TEMP_DIR=wandb_sdk_docs
 DESTINATION_DIR=python-library
-### HUGO_DIR=/Users/noahluna/Desktop/docs/content/ref/
-#HUGO_DIR=/Users/noahluna/Desktop/staging/ref/
 
-
-# Check if the script is run from the correct directory
-if [ ! -d "$TEMP_DIR" ]; then
-  mkdir -p "$TEMP_DIR"
-  echo "Directory '$TEMP_DIR' created."
+# Check if the directory exists, if it does, remove it else create it
+if [ -d "$TEMP_DIR" ]; then
+  echo "Directory '$TEMP_DIR' already exists. Removing it."
+  rm -rf "$TEMP_DIR"
 else
-  echo "Directory '$TEMP_DIR' already exists."
+  echo "Directory '$TEMP_DIR' does not exist. Creating it."
 fi
 
-# Check if the destination directory exists, if not, create it
-if [ ! -d "$DESTINATION_DIR" ]; then
-  mkdir -p "$DESTINATION_DIR"
-  echo "Directory '$DESTINATION_DIR' created."
+# Check if the destination directory exists, if it does, remove it else create it
+if [ -d "$DESTINATION_DIR" ]; then
+  echo "Directory '$DESTINATION_DIR' already exists. Removing it."
+  rm -rf "$DESTINATION_DIR"
 else
-  echo "Directory '$DESTINATION_DIR' already exists."
+  echo "Directory '$DESTINATION_DIR' does not exist. Creating it."
 fi
-
-
-# # Remove current docs in sdk_docs_temp
-# rm -rf $TEMP_DIR/*.md
-
-# # Remove current docs in Hugo directory
-# rm -rf $HUGO_DIR/$DESTINATION_DIR
 
 # Generate SDK docs using lazydocs
 python generate_sdk_docs.py --temp_output_directory=$TEMP_DIR
@@ -43,7 +33,3 @@ python sort_markdown_files.py --source_directory=$TEMP_DIR --destination_directo
 
 # Create _index.md files
 python create_landing_pages.py --source_directory=$DESTINATION_DIR
-
-# Move local file with subdirs to Hugo directory
-#echo "Moving files to Hugo directory"
-#mv $DESTINATION_DIR $HUGO_DIR
